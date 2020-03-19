159e9fc8d3f94eef929a5eaac03364982b6588e3
Vilmos Kintera <DAXRunBase@users.noreply.github.com>
2020-03-19 15:39:05 +0100

    Add files via upload

---
+++ Archiving SQL on Azure Blob Storage/AzCopy.ps1
@@ -0,0 +1,34 @@
+﻿# Clean up previous AzCopy journal executions
+Remove-Item "$env:USERPROFILE\AppData\Local\Microsoft\Azure\AzCopy\*.jnl"
+
+# Reset Archive bits
+#.\Clear-ArchiveBit.ps1 -Filter "*.bak" -Path "U:\Backup" -Recurse
+#.\Clear-ArchiveBit.ps1 -Filter "*.trn" -Path "U:\Backup" -Recurse
+
+# /@ Use parameter file
+# /S Recursive processing
+# /Y No prompt
+# Table type blob only: /EntityOperation:"InsertOrSkip"
+# /XO exclude same or older blob
+# /BlobType:block
+
+# get reference date (8th oldest full backup file)
+$dir = 'U:\Backup\AlwaysOnCluster\AXDatabaseName\FULL\'
+$latest = Get-ChildItem -Path $dir | Sort-Object CreationTime -Descending | Select-Object -Skip 7 -First 1
+$RefDate = $latest.CreationTime
+if(!$RefDate)
+{
+    $RefDate = Get-Date
+}
+
+# Copy transaction logs
+$args = '/@:"U:\Backup\AzCopyTRN.txt" /V:"U:\Backup\AzCopy_' + "$(Get-Date -Format 'yyyy-MM-dd').txt"
+$p = Start-Process 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' -ArgumentList $args -wait
+
+# Copy backup files
+$args = '/@:"U:\Backup\AzCopyBAK.txt" /V:"U:\Backup\AzCopy_' + "$(Get-Date -Format 'yyyy-MM-dd').txt"
+$p = Start-Process 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' -ArgumentList $args -wait
+
+# Remove files which were successfully copied
+$dir = 'U:\Backup\ENF-DAXCluster1$DAXSQLAG\AxaptaLive\' 
+Get-ChildItem $Dir -Include '*.bak','*.trn' -Recurse | Where-Object { $_.CreationTime -lt $RefDate } | Remove-Item
---
+++ Archiving SQL on Azure Blob Storage/MoveSQLBackups.ps1
@@ -0,0 +1,34 @@
+﻿$host.UI.RawUI.BufferSize = new-object System.Management.Automation.Host.Size(512,80)
+$VerbosePreference = 'Continue'
+
+Start-Transcript -Path "U:\backuplog.txt" -Append -Verbose
+
+$totalElapsedTime = [System.Diagnostics.Stopwatch]::StartNew()
+$sourceFolder = 'U:\Backup'
+$targetFolder = '\\NetworkStorageUNCPATH\Backup'
+
+$sourceInstances = Get-ChildItem -Directory $sourceFolder
+
+foreach ($instance in $sourceInstances)
+{
+    $sourceInstance = Join-Path -Path $sourceFolder -ChildPath $instance
+    $sourceDatabases = Get-ChildItem -Directory $sourceInstance
+
+    foreach ($database in $sourceDatabases)
+    {
+        $ElapsedTime = [System.Diagnostics.Stopwatch]::StartNew()
+        $sourceDatabase = Join-Path -Path $sourceInstance -ChildPath $database
+        $targetDatabase = Join-Path -Path $targetFolder -ChildPath $instance\$database
+        Write-Host "Moving $sourceDatabase\* to $targetDatabase ... " -NoNewline
+        Write-Output "Moving $sourceDatabase\* to $targetDatabase ... "
+        Move-Item -Path "$sourceDatabase\*" -Destination $targetDatabase | Write-Output
+        Write-Host "Done" -ForegroundColor Yellow | Write-Output
+ 	    Write-Host "Elapsed Time: $($ElapsedTime.Elapsed.ToString())"
+ 	    Write-Output "Elapsed Time: $($ElapsedTime.Elapsed.ToString())"
+    }
+}
+
+Write-Host "Total Elapsed Time: $($totalElapsedTime.Elapsed.ToString())"
+Write-Output "Total Elapsed Time: $($totalElapsedTime.Elapsed.ToString())"
+
+Stop-Transcript
