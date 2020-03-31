b966d7f1a88b88576c8323b504dbcea88d839e25
Vilmos Kintera <DAXRunBase@users.noreply.github.com>
2020-03-31 13:12:41 +0200

    Add files via upload

---
+++ Dynamics 365 Compile results/WIK_BuildErrorList.ps1
@@ -0,0 +1,49 @@
+﻿cls
+
+$displayErrorsOnly = $false # $true # $false
+$rootDirectory = "C:\AOSService\PackagesLocalDirectory"
+
+$results = Get-ChildItem -Path $rootDirectory -Filter BuildModelResult.log -Recurse -Depth 1 -ErrorAction SilentlyContinue -Force
+$totalErrors = 0
+$totalWarnings = 0
+
+foreach ($result in $results)
+{
+    try
+    {
+        $errorText = Select-String -LiteralPath $result.FullName -Pattern ^Errors: | ForEach-Object {$_.Line}
+        $errorCount = [int]$errorText.Split()[-1]
+        $totalErrors += $errorCount
+
+        $warningText = Select-String -LiteralPath $result.FullName -Pattern ^Warnings: | ForEach-Object {$_.Line}
+        $warningCount = [int]$warningText.Split()[-1]
+        $totalWarnings += $warningCount
+
+        if ($displayErrorsOnly -eq $true -and $errorCount -eq 0)
+        {
+            continue
+        }
+
+        Write-Host "$($result.DirectoryName)\$($result.Name) " -NoNewline
+        if ($errorCount -gt 0)
+        {
+            Write-Host " $errorText" -NoNewline -ForegroundColor Red
+        }
+        if ($warningCount -gt 0)
+        {
+            Write-Host " $warningText" -ForegroundColor Yellow
+        }
+        else
+        {
+            Write-Host
+        }
+    }
+    catch
+    {
+    Write-Host
+    Write-Host "Error during processing"
+    }
+}
+
+Write-Host "Total Errors: $totalErrors" -ForegroundColor Red
+Write-Host "Total Warnings: $totalWarnings" -ForegroundColor Yellow
---
+++ Dynamics 365 Compile results/WIK_BuildWarnings.ps1
@@ -0,0 +1,66 @@
+﻿cls
+
+#File processing
+$packageName = "JADOperation"
+
+$rootDirectory = "C:\AOSService\PackagesLocalDirectory\$packageName"
+$outputFile = "C:\Temp\" + $packageName + "-Warning.csv"
+
+$results = Get-ChildItem -Path $rootDirectory -Filter BuildModelResult.log -Recurse -ErrorAction SilentlyContinue -Force
+$objects = @()
+
+foreach ($result in $results)
+{
+    try
+    {
+        $errorText = Select-String -LiteralPath $result.FullName -Pattern '(^.*) Warning: (.*)' | ForEach-Object {$_.Line}
+        # Skip modules that do not have errors
+        if ($errorText)
+        {
+            Write-Host "Processing $($result.DirectoryName)\$($result.Name) "# -NoNewline
+        
+            foreach ($line in $errorText)
+            {
+                # Remove positioning text in the format of "[(5,5),(5,39)]: " for methods
+                if ($line -match '(?=\[\().*(?=\)\])')
+                {
+                    $lineReplaced = [regex]::Split($line, '(.*)(?=\[\().*(?:\)\]: )(.*)')
+                    $line = $lineReplaced[1] + $lineReplaced[2]
+                }
+
+                try
+                {
+                    # Regular expression matching to split line details into groups
+                    $regex = '(?:Compile Fatal|MetadataProvider|Metadata|Compile|Unspecified|Generation|ExternalReference|BestPractices) (Warning): (Query Method|Interface Method|Form Method LocalFunction|Form Control Method|Form Datasource Method|Form DataSource Method|Form DataSource DataField Method|Form Method|Map Method|Class Delegate|Table Method LocalFunction|Class Method LocalFunction|Table Method|Class Method|Table|Class|View|Form|)(?: |)(?:dynamics:|)(.*)(?:: )(.*)'
+                
+                    $Matches = [regex]::split($line, $regex)
+                    $object = [PSCustomObject]@{
+                        ErrorType  = $Matches[1].trim()
+                        ObjectType = $Matches[2].trim()
+                        Path       = $Matches[3].trim()
+                        Text       = $Matches[4].trim()
+                    }# | ft
+
+                    # Store all entries
+                    $objects += $object
+                }
+                catch
+                {
+                    Write-Host "Error during processing line <" -ForegroundColor Yellow -NoNewline
+                    Write-Host "$line" -ForegroundColor Red -NoNewline
+                    Write-Host ">" -ForegroundColor Yellow
+                    #Write-Host $regex
+                }
+                #break
+            }
+        }
+    }
+    catch
+    {
+        Write-Host
+        Write-Host "Error during processing"
+    }
+}
+ 
+# Write output to CSV
+$objects | export-csv -Path $outputFile -NoTypeInformation
